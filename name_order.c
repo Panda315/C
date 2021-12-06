a88af5ab378bbf11ded6adf2948e839f67950a8c
unknown <prashantstha1110@gmail.com>
2021-12-06 21:31:25 +0545

    uploaded name_order.c

---
+++ name_order.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct{
+        char name[20];
+        int sn;
+    }student;
+    
+
+int main()
+{
+    printf("\e[1;1H\e[2J");
+    student s[5];
+    student *p=s;
+    printf("All small letters.\n\n");
+    for(int i=0;i<5;i++){
+        printf("Name: ");
+        gets(s[i].name);
+        printf("Symbol No: ");
+        scanf("%d",&s[i].sn);
+        getchar();
+        printf("\n");
+    }
+    printf("\n");
+
+    
+    for(int m=0;m<4;m++)
+    {
+        for(int n=0;n<5;n++)
+        {
+            int r=0;
+            r=strcmp(s[n].name,s[n+1].name);                  //comparing string
+            
+            if(r==0)                                        //if both the names are same
+            {
+                if(s[n].sn<s[n+1].sn)                   //arranging in order of symbol  no if names are same
+                    continue;
+                    
+                else
+                {
+                    student temp=s[n];
+                    s[n]=s[n+1];
+                    s[n+1]=temp;
+                }
+            }
+            
+            else if(r>1)                                    //if alphabetically first name is greater than second
+            {
+                    student temp=s[n];
+                    s[n]=s[n+1];
+                    s[n+1]=temp;
+            }
+            
+            else                                            //if alphabetically first name is smaller than second
+                continue;
+        }
+    }
+    
+    printf("ROLL NO\t SYMBOL NO\t NAME\n");
+    for(int x=0;x<5;x++)
+    {
+        printf("%d\t",x+1);
+        printf("   %d\t",s[x].sn);
+        printf("\t%s\n",s[x].name);
+    }  
+}
