920f0545e3cb2359ff0e4f5ab7c5e5bfa59a09ce
unknown <prashantstha1110@gmail.com>
2021-12-08 22:03:33 +0545

    Added round function

--- armstrong_number.c
+++ armstrong_number.c
@@ -15,15 +15,19 @@ int main()
                 length++;
                 alter=alter/10;                  //counting the number of digits entered by user
         }while(alter>0);
-        
+        printf("%d",length);
         while(dup>0){
-                rem=dup%10;                     //isolating the last digit of the number
-                sum=sum+pow(rem,length);        
+                rem=dup%10;
+                int a=round(pow(rem,length));  
+                printf("\n%d",a) ;                  //isolating the last digit of the number
+                sum=sum+round(pow(rem,length));        
                 dup=dup/10;
         }                                       /*calculates the sum of digits of the number raised to the power equal
                                                 to the numbers of digits in the number*/
+            int b=pow(5,3);
+            printf("\n%d",b);
         if(num==sum){
-                printf("The number %d is a Armstrong number.",num);
+                printf("\nThe number %d is a Armstrong number.",num);
         }
         else
                 printf("The number  %d is not a Armstrong number.", num);
